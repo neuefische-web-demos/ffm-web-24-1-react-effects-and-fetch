bcace4e226fa3fc79dcbb8d4ce600832ded55de8
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-04-17 10:19:25 +0200

    initial commit

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Joke() {
+  const [joke, setJoke] = useState();
+  const [id, setId] = useState(0);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        `https://example-apis.vercel.app/api/bad-jokes/${id}`
+      );
+      const data = await response.json();
+
+      setJoke(data);
+    }
+    startFetching();
+  }, [id]);
+
+  if (!joke) {
+    return null;
+  }
+
   return (
     <>
-      <h1>Insert joke here</h1>
-      <button type="button">Next Joke</button>
+      <h1>{joke.joke}</h1>
+      <button type="button" onClick={()=>setId(joke.nextId)}>Next Joke</button>
     </>
   );
 }
+  
